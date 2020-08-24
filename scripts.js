e61eabf1c20da10056fbce744f894cd31ce19a49
Scott Griffin <mscottgriffin@gmail.com>
2020-08-24 13:47:36 -0400

    top section done, smooth scroll chevron works. Aligning bio card now

--- scripts.js
+++ scripts.js
@@ -16,9 +16,10 @@ window.onload = () => {
   typewriter()
 }
 
-// // const scrollToNext= () =>{
-  
-// // }
-
-
-// onClick="document.getElementById('gobtn').scrollIntoView('sectiontwo');" 
+const scrollto =(element) => {
+  // get the element on the page related to the button
+  var scrollToId = element.getAttribute("data-scroll");
+  var scrollToElement = document.getElementById(scrollToId);
+  // make the page scroll down to where you want
+  // ...
+}
