37875a0eb54471a22e15a3565d1d138aef7dcabf
Scott Griffin <mscottgriffin@gmail.com>
2020-08-24 22:33:19 -0400

    bio text

--- scripts.js
+++ scripts.js
@@ -16,10 +16,3 @@ window.onload = () => {
   typewriter()
 }
 
-const scrollto =(element) => {
-  // get the element on the page related to the button
-  var scrollToId = element.getAttribute("data-scroll");
-  var scrollToElement = document.getElementById(scrollToId);
-  // make the page scroll down to where you want
-  // ...
-}
